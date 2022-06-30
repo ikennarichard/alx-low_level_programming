526e017a1de86e3e3efeecdc3a865a714face360
Ikenna R O <oguejioforrichard@gmail.com>
2022-06-30 14:40:12 -0700

    add program to print string

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*
+ * main - Program start
+ *
+ * Description: Prints a string using the printf command
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
