5e19a1744d7ccda479b8bcf43c19377620a9e588
Ikenna R O <oguejioforrichard@gmail.com>
2022-06-30 14:43:07 -0700

    fix mai descriptionn and add newline

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /*
- * main - Program start
+ * main: Program main start
  *
  * Description: Prints a string using the printf command
  *
@@ -8,6 +8,6 @@
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
