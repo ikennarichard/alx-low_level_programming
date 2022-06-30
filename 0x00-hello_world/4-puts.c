7f8f34316e3448b3de1b16662a1f468b8bec6a57
Ikenna R O <oguejioforrichard@gmail.com>
2022-06-30 14:21:42 -0700

    add main descritpion

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-/* Description: This C program will print a strinng using the puts function*/
+/**
+ * main - The entry point of our program
+
+ * Description: This C program will print a strinng using the puts function
+ 
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
