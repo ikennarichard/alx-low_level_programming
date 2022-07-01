8584235224fdf7d0d820b9422bc2cb39385f8a8b
Ikenna R O <oguejioforrichard@gmail.com>
2022-07-01 14:22:18 -0700

    add new program

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - The starte of the program
+ *
+ * Description: Print no of base 16 in lowercase
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num = 48;
+
+	while (num <= 102)
+	{
+		putchar(num);
+
+		if (num == 57)
+			num+=39
+		++num;
+	}
+	putchar('\n');
+	return (0);
+}
