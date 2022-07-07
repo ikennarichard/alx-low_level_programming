d94ec6ff50f71afd89cf36157171c1441ffd6a6b
Ikenna R O <oguejioforrichard@gmail.com>
2022-07-07 10:25:50 -0700

    add 2 new programs

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+/**
+ * _sqrt - finds the square root
+ *
+ * @x: input
+ *
+ * Return: square root of x
+ */
+
+double _sqrt(dounble x)
+{
+	float sqrt, tmp;
+
+	sqrt = x / 2;
+	tmp = 0;
+
+	while (sqrt != tmp)
+	{
+		tmp = sqrt;
+		sqrt = (x / tmp + tmp) / 2;
+	}
+	return (sqrt);
+}
+
+/**
+ * prime_factor - finds and prints the largets prime factor of a number
+ *
+ * @num: number to find
+ */
+
+void prime_factor(long int num)
+{
+	int prmNu, largest;
+
+	while (num % 2 == 0)
+		num = num / 2;
+
+	for (prmNu = 3; prmnu <= _sqrt(num); prmNu +=2)
+	{
+		while (num % prmNu == 0)
+		{
+			num = num / prmNu;
+			largest = prmNu;
+		}
+	}
+
+	if (num > 2)
+		largest = num;
+	printf("%d\n", largest);
+}
+
+/**
+ * main - Satrt point
+ *
+ * Return: 0
+ */
+
+
+int main(void)
+{
+	prime_factor(612852475143);
+
+	return (0);
+}
+		
