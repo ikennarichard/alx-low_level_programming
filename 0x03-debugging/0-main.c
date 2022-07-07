15d700c7d2cbfb626a92e3f587974e7880250294
Ikenna R O <oguejioforrichard@gmail.com>
2022-07-07 06:55:29 -0700

    fix compile errors

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
- * main - test function possitiveornegative for correct output
- * Description: test for negative and positive
+ * main - test function that prints if integer is positive or negative
  * Return: 0
  */
 int main(void)
