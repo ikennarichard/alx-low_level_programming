ab1fbf9fd7071ededa5bfdc3160b537bd544c01d
Ikenna R O <oguejioforrichard@gmail.com>
2022-07-13 00:41:28 -0700

    Program that prints elements of an array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_array - prints n elements of array of integers
+ *
+ * @n: elements input
+ * @a: string input
+ *
+ * Return: Nothing
+ */
+
+void print_array(int *a, int n)
+{
+	int a;
+
+	for (a = 0; a < n; ++a)
+	{
+		if(a != (n - i))
+			printf("%d, ", a[i]);
+		else
+			printf("%d", a[i]);
+	}
+	printf("\n");
+}
