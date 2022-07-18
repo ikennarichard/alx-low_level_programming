f8e93e48a57f688650a196c435118a7ee2818cb2
Ikenna R O <oguejioforrichard@gmail.com>
2022-07-18 11:06:13 -0700

    Add add advanced task

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include  "main.h"
+
+/**
+ * set_srting - function that sets the value of a pointer to char
+ * @s: address
+ * @to: target address
+ *
+ * Return: Nothing
+ */
+void set_string(char ***s, char *to)
+{
+	*s = to;
+}
