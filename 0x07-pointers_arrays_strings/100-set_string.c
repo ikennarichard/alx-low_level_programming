62de9c46d8658dbb4033b4b83f2ab4d2200e0302
Ikenna R O <oguejioforrichard@gmail.com>
2022-07-18 11:12:32 -0700

    Fix requriement errors

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,8 +1,8 @@
 #include  "main.h"
 
 /**
- * set_srting - function that sets the value of a pointer to char
- * @s: address
+ * set_srting - sets the value of a pointer to char
+ * @s: source input
  * @to: target address
  *
  * Return: Nothing
