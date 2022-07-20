a28a261ac71e6f06fdb488277cc7bf96f42cea55
Ikenna R O <oguejioforrichard@gmail.com>
2022-07-20 07:19:51 -0700

    Remove whitespace in line 4

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * _puts_recursion - function that prints a string 
+ * _puts_recursion - function that prints a string
  * @s: string
+ *
  * Return: Nothing.
  */
 void _puts_recursion(char *s)
